0d450a71829b7eb505bdb5e1c8af7b4624ac840e
Dom <daumantas.pyragas@mail.bcu.ac.uk>
2020-06-07 14:44:02 +0100

    Updates

---
+++ precache-manifest.f6c2455ae1c3ff94dda61bd4ef07153f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b875726903e5f8a37daef81cf4e1dd2c",
+    "url": "/dpyragas/facerecognition/index.html"
+  },
+  {
+    "revision": "fd846d84fb95936e3c06",
+    "url": "/dpyragas/facerecognition/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "f19f2c8e833c0243ce6e",
+    "url": "/dpyragas/facerecognition/static/css/main.e89b5afc.chunk.css"
+  },
+  {
+    "revision": "fd846d84fb95936e3c06",
+    "url": "/dpyragas/facerecognition/static/js/2.b9913275.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dpyragas/facerecognition/static/js/2.b9913275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f19f2c8e833c0243ce6e",
+    "url": "/dpyragas/facerecognition/static/js/main.4aa5cae3.chunk.js"
+  },
+  {
+    "revision": "323863e9b531dfc52e69",
+    "url": "/dpyragas/facerecognition/static/js/runtime-main.edce4ace.js"
+  },
+  {
+    "revision": "982786b2da4fd556566aa3d7a53413b7",
+    "url": "/dpyragas/facerecognition/static/media/logo.982786b2.svg"
+  }
+]);
